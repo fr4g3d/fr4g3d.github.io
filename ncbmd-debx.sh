16c10240bdd5a5630abccc22abe0da4a1dfa5653
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2021-09-22 15:07:45 +0700

    Update-210922-1507

--- ncbmd-debx.sh
+++ ncbmd-debx.sh
@@ -6,14 +6,17 @@ clear
 #
 
 # disabling ipv6
-printf "\n" >> /etc/sysctl.conf
-printf "net.ipv6.conf.all.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-printf "net.ipv6.conf.default.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-printf "net.ipv6.conf.lo.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-printf "net.ipv6.conf.tun0.disable_ipv6 = 1\n" >> /etc/sysctl.conf
+sudo printf "\n" >> /etc/sysctl.conf
+sudo printf "net.ipv6.conf.all.disable_ipv6 = 1\n" >> /etc/sysctl.conf
+sudo printf "net.ipv6.conf.default.disable_ipv6 = 1\n" >> /etc/sysctl.conf
+sudo printf "net.ipv6.conf.lo.disable_ipv6 = 1\n" >> /etc/sysctl.conf
+sudo printf "net.ipv6.conf.tun0.disable_ipv6 = 1\n" >> /etc/sysctl.conf
 sudo service networking reload
 
 # install common apps.
 sudo apt update
 sleep 1
 sudo apt install openssl openssh-server openssh-client curl wget zip unzip
+wget https://github.com/nirui/sshwifty/releases/download/0.2.14-beta-release-prebuild/sshwifty_0.2.14-beta-release_linux_amd64.tar.gz
+tar -xf sshwifty_0.2.14-beta-release_linux_amd64.tar.gz -C sshwifty/
+cd sshwifty/
