7738eaead4e33d279baaba40fedbefa7f8b86ca1
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2021-09-22 16:30:37 +0700

    Update-210922-1630

--- ncbmd-debx.sh
+++ ncbmd-debx.sh
@@ -16,7 +16,7 @@ sudo service networking reload
 # install common apps.
 sudo apt update
 sleep 2
-sudo apt install openssl openssh-server openssh-client curl wget aria2 zip unzip
+sudo apt install dnsutils openssl openssh-server openssh-client curl wget aria2 zip unzip
 aria2c -d dlds/ -c -x 16 https://github.com/nirui/sshwifty/releases/download/0.2.14-beta-release-prebuild/sshwifty_0.2.14-beta-release_linux_amd64.tar.gz
 mkdir sshwifty/
 tar -xf dlds/sshwifty_0.2.14-beta-release_linux_amd64.tar.gz -C ~/sshwifty/
