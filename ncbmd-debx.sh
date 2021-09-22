23458e0ee0ad0e0ffff2ef0a5c8d362143d7cc35
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2021-09-22 16:14:20 +0700

    Update-210922-1614

--- ncbmd-debx.sh
+++ ncbmd-debx.sh
@@ -15,9 +15,33 @@ sudo service networking reload
 
 # install common apps.
 sudo apt update
-sleep 1
+sleep 2
 sudo apt install openssl openssh-server openssh-client curl wget aria2 zip unzip
 aria2c -d dlds/ -c -x 16 https://github.com/nirui/sshwifty/releases/download/0.2.14-beta-release-prebuild/sshwifty_0.2.14-beta-release_linux_amd64.tar.gz
 mkdir sshwifty/
 tar -xf dlds/sshwifty_0.2.14-beta-release_linux_amd64.tar.gz -C ~/sshwifty/
 cd sshwifty/
+chmod +x sshwifty_linux_amd64
+sudo install sshwifty_linux_amd64 /usr/local/bin/sshwifty
+cp sshwifty.conf.example.json ~/.config/sshwifty.conf.json
+cd 
+printf "[Unit]" > sshwifty.service
+printf "Description=SSH Wifty Service." >> sshwifty.service
+printf "After=network.target" >> sshwifty.service
+printf "StartLimitIntervalSec=0" >> sshwifty.service
+printf "\n" >> sshwifty.service
+printf "[Service]" >> sshwifty.service
+printf "Type=simple" >> sshwifty.service
+printf "Restart=always" >> sshwifty.service
+printf "RestartSec=1" >> sshwifty.service
+printf "User=aset" >> sshwifty.service
+printf "ExecStart=/usr/local/bin/sshwifty" >> sshwifty.service
+printf "\n" >> sshwifty.service
+printf "[Install]" >> sshwifty.service
+printf "WantedBy=multi-user.target" >> sshwifty.service
+printf "\n" >> sshwifty.service
+sudo mv sshwifty.service /etc/systemd/system/sshwifty.service
+sudo systemctl enable sshwifty.service
+sudo service sshwifty start
+sleep 3
+clear
