7847986f4f739784123dfc7c67a94d5d6b827ae0
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-01-17 11:17:54 +0700

    Update 2023-01-17-11-17
    - Complete Installer;
    - Update title index.html;
    - Add favicon.ico;
    - Update All Scripts.

--- ncbmd-debx.sh
+++ ncbmd-debx.sh
@@ -32,7 +32,7 @@ mkdir .config/
 cd sshwifty/
 chmod +x sshwifty_linux_amd64
 sudo install sshwifty_linux_amd64 /usr/local/bin/sshwifty
-aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/sshwifty.conf.json
+aria2c -c -x 16 https://fr4g3d.github.io/sshwifty.conf.json
 sudo cp sshwifty.conf.json ~/.config/sshwifty.conf.json
 cd 
 printf "[Unit]\n" > sshwifty.service
@@ -53,48 +53,5 @@ printf "\n" >> sshwifty.service
 sudo mv sshwifty.service /etc/systemd/system/sshwifty.service
 sudo systemctl enable sshwifty.service
 sudo service sshwifty start
-sleep 3
-# install apache2 mod-php openssl.
-sudo apt update
-sudo apt -y install apache2 apache2-doc libapache2-mod-php openssl 
-#sudo apt install apache2 apache2-doc libapache2-mod-php openssl 
-sudo a2enmod headers
-sudo a2enmod env
-sudo a2enmod dir
-sudo a2enmod mime
-sudo a2enmod rewrite
-sudo a2enmod setenvif
-sudo a2enmod ssl
-sudo a2ensite default-ssl
-sudo service apache2 reload
-sleep 2
-# install php php-commons.
-sudo apt -y install php php-fpm php-common php-xml php-curl php-gd php-json php-mbstring php-zip php-sqlite* php-mysql* php-pgsql* php-bz2 php-intl php-ldap php-imap php-bcmath php-gmp php-redis php-imagick
-#sudo apt install php php-common php-xml php-curl php-gd php-json php-mbstring php-zip php-mysql php-bz2 php-intl php-ldap php-smbclient php-imap php-bcmath php-gmp php-redis php-imagick
-sudo apt -y install smbclient cifs-utils libsmbclient-dev php-dev make
-sudo pecl install smbclient
-sudo sh -c "printf 'extension=smbclient.so' >> /etc/php/7.4/fpm/php.ini"
-sudo systemctl restart php7.4-fpm
-sudo php -v
-sleep 2
-# install mariadb-server as mysql-server.
-sudo apt -y install mariadb-server mariadb-client
-sleep 2
-printf "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'admin123' WITH GRANT OPTION;\n" > mysqld.sql
-printf "GRANT ALL PRIVILEGES ON *.* TO 'aset'@'localhost' IDENTIFIED BY 'aset123' WITH GRANT OPTION;\n" >> mysqld.sql
-printf "FLUSH PRIVILEGES;\n\\q"  >> mysqld.sql
-printf "type \"source mysqld.sql\"\n"
-sleep 2
-sudo mysql -uroot < mysqld.sql
-sleep 2
-# install common for installing Common Downloader.
-sudo apt -y install curl wget zip unzip aria2 ffmpeg
-sleep 2
-aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/pfm.php
-aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/xp.php
-sudo mkdir /var/www/html/.apps
-sudo mv dlds/pfm.php /var/www/html/.apps/
-sudo mv dlds/xp.php /var/www/html/.apps/
 sleep 2
 echo Done.
-sleep 3
--- tio-installer.sh
+++ tio-installer.sh
@@ -11,10 +11,12 @@ sudo apt update
 sudo apt -y install curl wget zip unzip aria2 ffmpeg
 sleep 2
 aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/ncbmd-debx.sh
+aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/install-lamp.sh
 aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/install-livehelperchat.sh
 aria2c -d dlds/ -c -x 16 https://fr4g3d.github.io/install-nextcloud.sh
 sudo chmod +x dlds/*.sh
 sudo ./dlds/ncbmd-debx.sh
+sudo ./dlds/install-lamp.sh
 sudo ./dlds/install-livehelperchat.sh
 sudo ./dlds/install-nextcloud.sh
 sleep 2
