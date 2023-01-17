7aa6ec280a1cdd74ef68eb34ff72f513a9ed28b1
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-01-17 10:27:24 +0700

    Update 2023-01-17-10-27
    - Complete Installer;
    - Update title index.html;
    - Add favicon.ico;
    - Update All Scripts.

--- ncbmd-debx.sh
+++ ncbmd-debx.sh
@@ -6,11 +6,11 @@ clear
 #
 
 # disabling ipv6
-sudo printf "\n" >> /etc/sysctl.conf
-sudo printf "net.ipv6.conf.all.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-sudo printf "net.ipv6.conf.default.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-sudo printf "net.ipv6.conf.lo.disable_ipv6 = 1\n" >> /etc/sysctl.conf
-sudo printf "net.ipv6.conf.tun0.disable_ipv6 = 1\n" >> /etc/sysctl.conf
+sudo sh -c "printf \"\n\" >> /etc/sysctl.conf"
+sudo sh -c "printf \"net.ipv6.conf.all.disable_ipv6 = 1\n\" >> /etc/sysctl.conf"
+sudo sh -c "printf \"net.ipv6.conf.default.disable_ipv6 = 1\n\" >> /etc/sysctl.conf"
+sudo sh -c "printf \"net.ipv6.conf.lo.disable_ipv6 = 1\n\" >> /etc/sysctl.conf"
+sudo sh -c "printf \"net.ipv6.conf.tun0.disable_ipv6 = 1\n\" >> /etc/sysctl.conf"
 sudo service networking reload
 
 # install common apps.
