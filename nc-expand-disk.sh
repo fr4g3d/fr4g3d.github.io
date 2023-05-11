714bfcbeb76358b60eabc5d2a140ce3355d16bb2
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-05-12 01:05:57 +0700

    Update 2023-05-12-01-05
    -nc-expand-disk.sh : added

---
+++ nc-expand-disk.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Nextcloud v.21.0.4 Installer for ncmbd-debx Server.
+#
+clear
+#
+
+# install apache2 mod-php openssl.
+sudo apt update
+sudo apt -y install fdisk mhddfs
+sudo fdisk /dev/sdb
+sudo mkfs.ext4 /dev/sdb1
+sudo mkdir -p /mnt/ncd0
+sudo mount -o defaults /dev/sdb1 /mnt/ncd0
+sudo printf "/dev/sdb1 /mnt/ncd0 ext4 defaults 0 2\n" | sudo tee -a /etc/fstab
+sudo mkdir -p /mnt/fncd
+sudo chown www-data /mnt/fncd
+sudo mhddfs /var/www/html/.apps/nextcloud/data,/mnt/ncd0 /mnt/fncd -o allow_other,nonempty
+sudo -u www-data php /var/www/html/.apps/nextcloud/occ maintenance:mode --on
+sudo nano /var/www/html/.apps/nextcloud/config/config.php
+sudo -u www-data php /var/www/html/.apps/nextcloud/occ maintenance:mode --off
+sudo printf "mhddfs#/var/www/html/.apps/nextcloud/data,/mnt/ncd0 /mnt/fncd fuse defaults,allow_other,nonempty 0 0\n" | sudo tee -a /etc/fstab
+clear
