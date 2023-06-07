4a4915c8625c9afb939c00facdd92eabc62fcae5
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-06-07 16:24:45 +0700

    Update-2023-06-07-16-25
    Update New.

---
+++ install-lamp-termux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Install LAMP Server.
+#
+clear
+#
+pkg update
+termux-change-repo
+pkg install wget git
+#rm php_7.4.12_aarch64.deb
+#rm php-apache_7.4.12_aarch64.deb
+#wget https://github.com/Termux-pod/termux-pod/raw/main/aarch64/Php7/php_7.4.12_aarch64.deb
+#wget https://github.com/Termux-pod/termux-pod/raw/main/aarch64/Php7/php-apache_7.4.12_aarch64.deb
+#dpkg -i php_7.4.12_aarch64.deb
+#dpkg -i php-apache_7.4.12_aarch64.deb
+apt --fix-missing --fix-broken install
+pkg install php-apache php apache2 mariadb -y -f
+wget https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
+tar xzvf dokuwiki-stable.tgz
+mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
+mv dokuwiki/* $PREFIX/share/apache2/default-site/htdocs/dw/
+httpd &
+mysqld &
+printf "Done..."
