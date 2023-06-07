5610511a778a47f06fa74b39deb0df185cbf9a73
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-06-07 22:10:02 +0700

    Update-2023-06-07-22-10
    Update:
    install-lamp-termux.sh;
    index.html.

--- install-lamp-termux.sh
+++ install-lamp-termux.sh
@@ -14,11 +14,20 @@ pkg install openssl openssh wget git -y
 #dpkg -i php_7.4.12_aarch64.deb
 #dpkg -i php-apache_7.4.12_aarch64.deb
 apt --fix-missing --fix-broken install
-pkg install php-apache php apache2 mariadb -y -f
+pkg install php php-apache apache2 mariadb -y -f
 wget https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
-tar xzvf dokuwiki-stable.tgz
+tar -xzvf dokuwiki-stable.tgz
 mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
 mv dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
+printf "LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so" >> $PREFIX/etc/apache2/httpd.conf
+printf "LoadModule php_module libexec/apache2/libphp.so" >> $PREFIX/etc/apache2/httpd.conf
+printf "<IfModule dir_module>" >> $PREFIX/etc/apache2/httpd.conf
+printf "  DirectoryIndex index.html index.php" >> $PREFIX/etc/apache2/httpd.conf
+printf "</IfModule>" >> $PREFIX/etc/apache2/httpd.conf
+printf "</IfModule>" >> $PREFIX/etc/apache2/httpd.conf
+printf "<FilesMatch \.php$>" >> $PREFIX/etc/apache2/httpd.conf
+printf "    SetHandler application/x-httpd-php" >> $PREFIX/etc/apache2/httpd.conf
+printf "</FilesMatch>" >> $PREFIX/etc/apache2/httpd.conf
 httpd &
 mysqld &
 printf "Done..."
