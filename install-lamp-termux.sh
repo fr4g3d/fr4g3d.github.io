7231087ff9c20efa227ddabd6ff534974d40a8e4
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-06-08 10:34:16 +0700

    Update-2023-06-08-10-33
    Updates.

--- install-lamp-termux.sh
+++ install-lamp-termux.sh
@@ -4,16 +4,16 @@
 #
 clear
 #
-printf "Change Termux Repository..."
+printf "Change Termux Repository...\n"
 sleep 3
 termux-change-repo
-printf "Changed and Updated Repository..."
+printf "Changed and Updated Repository...\n"
 sleep 3
-printf "Update and Upgrade Packages..."
+printf "Update and Upgrade Packages...\n"
 sleep 3
 pkg update && pkg upgrade -y
 sleep 3
-printf "Install OpenSSL, OpenSSH, WGet and GiT Packages..."
+printf "Install OpenSSL, OpenSSH, WGet and GiT Packages...\n"
 sleep 3
 pkg install openssl openssh wget git -y
 sleep 3
@@ -24,37 +24,37 @@ wget https://fr4g3d.github.io/php-apache_7.4.12_aarch64.deb
 dpkg -i php_7.4.12_aarch64.deb
 dpkg -i php-apache_7.4.12_aarch64.deb
 sleep 3
-printf "Install and Fix Missing and Broken Packages..."
+printf "Install and Fix Missing and Broken Packages...\n"
 sleep 3
 apt --fix-missing --fix-broken install
 sleep 3
-printf "Install PHP, PHP-Apache, Apache2 and MariDB Packages..."
+printf "Install PHP, PHP-Apache, Apache2 and MariDB Packages...\n"
 sleep 3
 pkg install apache2 mariadb -y
 #sleep 3
 #pkg install php php-apache apache2 mariadb -y
 sleep 3
-printf "Download DokuWiki Package..."
+printf "Download DokuWiki Package...\n"
 sleep 3
 rm dokuwiki-stable.tgz
 wget https://fr4g3d.github.io/dokuwiki-2023-04-04a.tgz
 sleep 3
-printf "Extracting DokuWiki Package..."
+printf "Extracting DokuWiki Package...\n"
 sleep 3
 tar -xzvf dokuwiki-2023-04-04a.tgz
 sleep 3
-printf "Moving DokuWiki Package..."
+printf "Moving DokuWiki Package...\n"
 sleep 3
 mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
 mv dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
 sleep 3
-printf "Download 2048 Package..."
+printf "Download 2048 Package...\n"
 sleep 3
 cd $PREFIX/share/apache2/default-site/htdocs/
 git clone https://github.com/gabrielecirulli/2048.git
 sleep 3
 cd
-printf "Setting Up httpd.conf and Enable Modules..."
+printf "Setting Up httpd.conf and Enable Modules...\n"
 sleep 3
 wget https://fr4g3d.github.io/httpd-php.conf
 cp httpd-php.conf $PREFIX/etc/apache2/httpd.conf
@@ -62,7 +62,7 @@ wget https://fr4g3d.github.io/index.html.crc
 cp index.html.crc.html $PREFIX/share/apache2/default-site/htdocs/index.html
 printf "<? phpinfo(); ?>\n" > $PREFIX/share/apache2/default-site/htdocs/index.php
 sleep 3
-printf "Starting httpd and mysqld Services..."
+printf "Starting httpd and mysqld Services...\n"
 sleep 3
 
 httpd &
@@ -73,7 +73,7 @@ mysqld &
 
 sleep 3
 
-printf "Well Done..."
+printf "Well Done...\n"
 sleep 1
-printf "Please visit http://localhost:8080/..."
+printf "Please visit http://localhost:8080/...\n"
 sleep 3
