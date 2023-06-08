82d1ad71a1547d3b5d0dd5cfd64f7648e265a700
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-06-08 13:20:37 +0700

    Update-2023-06-08-13-20
    Update:
    - index.html;
    - install-lamp-termux.sh.
    Added:
    + ajax_cms.zip.

--- install-lamp-termux.sh
+++ install-lamp-termux.sh
@@ -17,38 +17,39 @@ printf "Install OpenSSL, OpenSSH, WGet and GiT Packages...\n"
 sleep 3
 pkg install openssl openssh wget git -y
 sleep 3
-rm php_7.4.12_aarch64.deb
-rm php-apache_7.4.12_aarch64.deb
-wget https://fr4g3d.github.io/php_7.4.12_aarch64.deb
-wget https://fr4g3d.github.io/php-apache_7.4.12_aarch64.deb
-dpkg -i php_7.4.12_aarch64.deb
-dpkg -i php-apache_7.4.12_aarch64.deb
+#rm php_7.4.12_aarch64.deb
+#rm php-apache_7.4.12_aarch64.deb
+#wget https://fr4g3d.github.io/php_7.4.12_aarch64.deb
+#wget https://fr4g3d.github.io/php-apache_7.4.12_aarch64.deb
+#dpkg -i php_7.4.12_aarch64.deb
+#dpkg -i php-apache_7.4.12_aarch64.deb
+#sleep 3
 sleep 3
 printf "Install and Fix Missing and Broken Packages...\n"
 sleep 3
 apt --fix-missing --fix-broken install
 sleep 3
 printf "Install PHP, PHP-Apache, Apache2 and MariDB Packages...\n"
-sleep 3
-pkg install apache2 mariadb -y
 #sleep 3
-#pkg install php php-apache apache2 mariadb -y
+#pkg install apache2 mariadb -y
 sleep 3
-printf "Download FlatPress Package...\n"
+pkg install php php-apache apache2 mariadb -y
+sleep 3
+printf "Download AjaxCMS Package...\n"
 sleep 3
 rm flatpress-1.2.1.zip
-wget https://fr4g3d.github.io/flatpress-1.2.1.zip
+wget https://fr4g3d.github.io/ajax_cms.zip
 sleep 3
-printf "Extracting FlatPress Package...\n"
+printf "Extracting AjaxCMS Package...\n"
 sleep 3
 unzip flatpress-1.2.1.zip
 sleep 3
-printf "Moving FlatPress Package...\n"
+printf "Moving AjaxCMS Package...\n"
 sleep 3
-mkdir $PREFIX/share/apache2/default-site/htdocs/fp/
-mv flatpress-*/* $PREFIX/share/apache2/default-site/htdocs/fp/
+mkdir $PREFIX/share/apache2/default-site/htdocs/ac/
+mv -f ajax_cms/* $PREFIX/share/apache2/default-site/htdocs/ac/
 sleep 3
-printf "Success: FlatPress Installed...\n"
+printf "Success: AjaxCMS Installed...\n"
 sleep 3
 printf "Download DokuWiki Package...\n"
 sleep 3
@@ -62,7 +63,7 @@ sleep 3
 printf "Moving DokuWiki Package...\n"
 sleep 3
 mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
-mv dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
+mv -f dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
 sleep 3
 printf "Success: DokuWiki Installed...\n"
 sleep 3
@@ -77,12 +78,19 @@ cd
 printf "Setting Up httpd.conf and Enable Modules...\n"
 sleep 3
 wget https://fr4g3d.github.io/httpd-php.conf
-cp -f httpd-php.conf $PREFIX/etc/apache2/httpd.conf
+mv -f httpd-php.conf $PREFIX/etc/apache2/httpd.conf
 wget https://fr4g3d.github.io/index.html.crc.html
-cp -f index.html.crc.html $PREFIX/share/apache2/default-site/htdocs/index.html
+mv -f index.html.crc.html $PREFIX/share/apache2/default-site/htdocs/index.html
 printf "<? phpinfo(); ?>\n" > $PREFIX/share/apache2/default-site/htdocs/index.php
 sleep 3
 printf "Starting httpd and mysqld Services...\n"
+sleep 3
+passwd
+
+sleep 3
+
+sshd &
+
 sleep 3
 
 httpd &
