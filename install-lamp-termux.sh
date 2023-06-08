8ba6797d784fa348f4c6380ef96fcabb8933ae48
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-06-08 08:46:41 +0700

    Update-2023-06-08-08-46
    Update:
    - install-lamp-termux.sh;
    - index.html.
    Added:
    + dokuwiki-2023-04-04a.tgz

--- install-lamp-termux.sh
+++ install-lamp-termux.sh
@@ -6,7 +6,9 @@ clear
 #
 termux-change-repo
 pkg update && pkg upgrade -y
+sleep 3
 pkg install openssl openssh wget git -y
+sleep 3
 #rm php_7.4.12_aarch64.deb
 #rm php-apache_7.4.12_aarch64.deb
 #wget https://github.com/Termux-pod/termux-pod/raw/main/aarch64/Php7/php_7.4.12_aarch64.deb
@@ -14,24 +16,29 @@ pkg install openssl openssh wget git -y
 #dpkg -i php_7.4.12_aarch64.deb
 #dpkg -i php-apache_7.4.12_aarch64.deb
 apt --fix-missing --fix-broken install
-pkg install php php-apache apache2 mariadb -y -f
+sleep 3
+pkg install php php-apache apache2 mariadb -y
+sleep 3
 rm dokuwiki-stable.tgz
-wget https://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
-tar -xzvf dokuwiki-stable.tgz
+wget https://fr4g3d.github.io/dokuwiki-2023-04-04a.tgz
+sleep 3
+tar -xzvf dokuwiki-2023-04-04a.tgz
+sleep 3
 mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
 mv dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
 cd $PREFIX/share/apache2/default-site/htdocs/
 git clone https://github.com/gabrielecirulli/2048.git
+sleep 3
 cd
-printf "LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so" >> $PREFIX/etc/apache2/httpd.conf
-printf "LoadModule php_module libexec/apache2/libphp.so" >> $PREFIX/etc/apache2/httpd.conf
-printf "<IfModule dir_module>" >> $PREFIX/etc/apache2/httpd.conf
-printf "  DirectoryIndex index.html index.php" >> $PREFIX/etc/apache2/httpd.conf
-printf "</IfModule>" >> $PREFIX/etc/apache2/httpd.conf
-printf "</IfModule>" >> $PREFIX/etc/apache2/httpd.conf
-printf "<FilesMatch \.php$>" >> $PREFIX/etc/apache2/httpd.conf
-printf "    SetHandler application/x-httpd-php" >> $PREFIX/etc/apache2/httpd.conf
-printf "</FilesMatch>" >> $PREFIX/etc/apache2/httpd.conf
+printf "LoadModule mpm_worker_module libexec/apache2/mod_mpm_worker.so\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "LoadModule php_module libexec/apache2/libphp.so\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "<IfModule dir_module>\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "  DirectoryIndex index.html index.php\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "</IfModule>\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "</IfModule>\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "<FilesMatch \.php$>\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "    SetHandler application/x-httpd-php\n" >> $PREFIX/etc/apache2/httpd.conf
+printf "</FilesMatch>\n" >> $PREFIX/etc/apache2/httpd.conf
 httpd &
 mysqld &
 printf "Done..."
