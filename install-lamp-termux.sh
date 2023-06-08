353a67c1d8f67f1ecf00aa0eeaabea2438735cc2
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-06-08 11:09:56 +0700

    Update-2023-06-08-11-10
    Update:
    - index.html;
    - index.hml.crc.html;
    - install-lamp-termux.sh.
    Added:
    + flatpress-1.2.1.zip.

--- install-lamp-termux.sh
+++ install-lamp-termux.sh
@@ -34,6 +34,22 @@ pkg install apache2 mariadb -y
 #sleep 3
 #pkg install php php-apache apache2 mariadb -y
 sleep 3
+printf "Download FlatPress Package...\n"
+sleep 3
+rm flatpress-1.2.1.zip
+wget https://fr4g3d.github.io/flatpress-1.2.1.zip
+sleep 3
+printf "Extracting FlatPress Package...\n"
+sleep 3
+unzip flatpress-1.2.1.zip
+sleep 3
+printf "Moving FlatPress Package...\n"
+sleep 3
+mkdir $PREFIX/share/apache2/default-site/htdocs/fp/
+mv flatpress-*/* $PREFIX/share/apache2/default-site/htdocs/fp/
+sleep 3
+printf "Success: FlatPress Installed...\n"
+sleep 3
 printf "Download DokuWiki Package...\n"
 sleep 3
 rm dokuwiki-stable.tgz
@@ -48,18 +64,22 @@ sleep 3
 mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
 mv dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
 sleep 3
+printf "Success: DokuWiki Installed...\n"
+sleep 3
 printf "Download 2048 Package...\n"
 sleep 3
 cd $PREFIX/share/apache2/default-site/htdocs/
 git clone https://github.com/gabrielecirulli/2048.git
 sleep 3
+printf "Success: 2048 Game Installed...\n"
+sleep 3
 cd
 printf "Setting Up httpd.conf and Enable Modules...\n"
 sleep 3
 wget https://fr4g3d.github.io/httpd-php.conf
-cp httpd-php.conf $PREFIX/etc/apache2/httpd.conf
-wget https://fr4g3d.github.io/index.html.crc
-cp index.html.crc.html $PREFIX/share/apache2/default-site/htdocs/index.html
+cp -f httpd-php.conf $PREFIX/etc/apache2/httpd.conf
+wget https://fr4g3d.github.io/index.html.crc.html
+cp -f index.html.crc.html $PREFIX/share/apache2/default-site/htdocs/index.html
 printf "<? phpinfo(); ?>\n" > $PREFIX/share/apache2/default-site/htdocs/index.php
 sleep 3
 printf "Starting httpd and mysqld Services...\n"
