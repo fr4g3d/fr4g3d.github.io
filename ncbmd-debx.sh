428bda5d804517e91744f36e2ca59d9c8b052b57
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-07-10 15:37:52 +0700

    Update ncbmd-debx.sh

--- ncbmd-debx.sh
+++ ncbmd-debx.sh
@@ -17,7 +17,7 @@ sudo service networking reload
 sudo apt update
 sleep 2
 sudo apt -y install dnsutils openssl openssh-server openssh-client shellinabox curl wget aria2 zip unzip
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/00_Green_On_Black.css
+aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/00_Green_On_Black.css
 sudo mv -f dlds/00_Green_On_Black.css /etc/shellinabox/options-available/
 sudo ln -s /etc/shellinabox/options-available/00_Green_On_Black.css /etc/shellinabox/options-enabled/00+Green_On_Black.css
 sudo mv -f "/etc/shellinabox/options-enabled/00+Black on White.css" "/etc/shellinabox/options-enabled/00_Black On White.css"
@@ -25,16 +25,16 @@ sudo sh -c "printf \"SHELLINABOX_ARGS=\"--disable-ssl\"\n\" >> /etc/default/shel
 sudo service shellinabox restart
 sleep 2
 sudo rm dlds/sshwifty_0.2.*.tar.gz
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sshwifty_0.2.22-beta-release_linux_amd64.tar.gz
+aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/sshwifty_0.2.22-beta-release_linux_amd64.tar.gz
 mkdir sshwifty/
 sudo tar -xf dlds/sshwifty_0.2.22-beta-release_linux_amd64.tar.gz -C ~/sshwifty/
 sleep 2
 mkdir .config/
 sudo chmod +x sshwifty/sshwifty_linux_amd64
 sudo install sshwifty/sshwifty_linux_amd64 /usr/local/bin/sshwifty
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sshwifty.conf.json
+aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/sshwifty.conf.json
 sleep 2
-sudo cp dlds/sshwifty.conf.json ~/.config/
+cp dlds/sshwifty.conf.json ~/.config/
 printf "[Unit]\n" > sshwifty.service
 printf "Description=SSH Wifty Service.\n" >> sshwifty.service
 printf "After=network.target\n" >> sshwifty.service
