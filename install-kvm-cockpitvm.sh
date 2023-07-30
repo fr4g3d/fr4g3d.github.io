c2294b1c71f993e280919f7f10778bb795f7750c
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-07-30 13:50:17 +0700

    Update-2023-07-30-13-50
    Update:
    - install-kvm-ckptvm.

--- install-kvm-cockpitvm.sh
+++ install-kvm-cockpitvm.sh
@@ -19,14 +19,14 @@ sudo service cockpit start
 sleep 2
 # install Cockpit VM App.
 sudo apt -y install cockpit cockpit-machines
-sleep 2
+#sleep 2
 # install Cockpit Navigator App.
-aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/cockpit-navigator_0.5.10-1focal_all.deb
-sudo apt -y install ./dlds/cockpit-navigator_0.5.10-1focal_all.deb
-sleep 2
+# aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/cockpit-navigator_0.5.10-1focal_all.deb
+# sudo apt -y install ./dlds/cockpit-navigator_0.5.10-1focal_all.deb
+# sleep 2
 # install Cockpit File-Sharing App.
-aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/cockpit-file-sharing_3.3.3-1focal_all.deb
-sudo apt -y install ./dlds/cockpit-file-sharing_3.3.3-1focal_all.deb
+#aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/cockpit-file-sharing_3.3.3-1focal_all.deb
+#sudo apt -y install ./dlds/cockpit-file-sharing_3.3.3-1focal_all.deb
 sleep 2
 echo Done.
 sleep 3
