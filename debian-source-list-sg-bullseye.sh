a3e8f53f55060335d6d25d8795a3f118925fa343
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-08-10 10:23:19 +0700

    Update-2023-08-10-10-23

---
+++ debian-source-list-sg-bullseye.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# Import APT Source.List
+#
+clear
+#
+
+printf "
+
+#
+deb  http://ftp.sg.debian.org/debian/ bullseye main non-free contrib
+deb-src http://ftp.sg.debian.org/debian/ bullseye main non-free contrib
+
+deb http://ftp.sg.debian.org/debian/ bullseye-updates main non-free contrib
+deb-src http://ftp.sg.debian.org/debian/ bullseye-updates main non-free contrib
+
+deb http://deb.debian.org/debian/ bullseye-backports main non-free contrib
+deb-src http://deb.debian.org/debian/ bullseye-backports main non-free contrib
+
+#deb http://ftp.sg.debian.org/debian/ unstable main non-free contrib
+#deb-src http://ftp.sg.debian.org/debian/ unstable main non-free contrib
+
+deb http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+deb-src http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+
+#
+#deb http://mirror.coganng.com/debian/ bullseye main non-free contrib
+#deb-src http://mirror.coganng.com/debian/ bullseye main non-free contrib
+
+#deb http://mirror.coganng.com/debian/ bullseye-updates main non-free contrib
+#deb-src http://mirror.coganng.com/debian/ bullseye-updates main non-free contrib
+
+#deb http://mirror.coganng.com/debian/ unstable main non-free contrib
+#deb-src http://mirror.coganng.com/debian/ unstable main non-free contrib
+
+#deb http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+#deb-src http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+
+#
+deb http://mirror.sg.gs/debian/ bullseye main non-free contrib
+deb-src http://mirror.sg.gs/debian/ bullseye main non-free contrib
+
+deb http://mirror.sg.gs/debian/ bullseye-updates main non-free contrib
+deb-src http://mirror.sg.gs/debian/ bullseye-updates main non-free contrib
+
+#deb http://mirror.sg.gs/debian/ unstable main non-free contrib
+#deb-src http://mirror.sg.gs/debian/ unstable main non-free contrib
+
+#deb http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+#deb-src http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+
+#
+#deb http://mirror.soonkeat.sg/debian/ bullseye main non-free contrib
+#deb-src http://mirror.soonkeat.sg/debian/ bullseye main non-free contrib
+
+#deb http://mirror.soonkeat.sg/debian/ bullseye-updates main non-free contrib
+#deb-src http://mirror.soonkeat.sg/debian/ bullseye-updates main non-free contrib
+
+#deb http://mirror.soonkeat.sg/debian/ unstable main non-free contrib
+#deb-src http://mirror.soonkeat.sg/debian/ unstable main non-free contrib
+
+#deb http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+#deb-src http://deb.debian.org/debian-security/ bullseye-security main non-free contrib
+" >> /etc/apt/source.list
