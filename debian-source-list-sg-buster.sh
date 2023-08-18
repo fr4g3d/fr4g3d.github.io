67a7fed1592e96b31961d5b7ea3df3d24dc2372a
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-08-19 05:51:27 +0700

    Update-2023-08-19-05-51

--- debian-source-list-sg-buster.sh
+++ debian-source-list-sg-buster.sh
@@ -65,6 +65,6 @@ deb-src http://mirror.sg.gs/debian/ buster-updates main contrib non-free
 
 #deb http://deb.debian.org/debian-security buster/updates main contrib non-free
 #deb-src http://deb.debian.org/debian-security buster/updates main contrib non-free
-\" >> /etc/apt/sources.list"
+\" >> /etc/apt/sources.list.d/deb-adds.list"
 sleep 2
 sudo apt update
