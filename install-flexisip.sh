072afae76f9d6b91e0e6b72c193debb9ef4ff842
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-08-19 12:23:39 +0700

    Update-2023-08-19-12-23

--- install-flexisip.sh
+++ install-flexisip.sh
@@ -32,8 +32,58 @@ sudo apt -y install bc-flexisip
 sleep 2
 /opt/belledonne-communications/bin/flexisip --dump-default all > flexisip.conf
 sleep 2
+sudo sh -c "printf \"#
+[global]
+debug=false
+aliases=sip.example.org localhost
+
+[module::Registrar]
+enabled=true
+reg-domains=sip.example.org localhost
+db-implementation=internal
+
+[module::Authentication]
+enabled=true
+auth-domains=sip.example.org localhost
+db-implementation=file
+file-path=/etc/flexisip/users.db.txt
+\" >> flexisip.conf"
+sleep 2
+sudo sh -c "printf \"#
+\" >> domain-registrations.conf"
+sleep 2
+sudo sh -c "printf \"#
+version:1
+
+admin@sip.example.org clrtxt:admin@123 ;
+user1@sip.example.org clrtxt:user1@123 ;
+\" >> users.db.txt"
+sleep 2
+# copy configs.
 sudo cp flexisip.conf /etc/flexisip/flexisip.conf
-exit
+sudo cp domain-registrations.conf /etc/flexisip/domain-registrations.conf
+sudo cp users.db.txt /etc/flexisip/users.db.txt
+sleep 2
+# Zero-FlexiSiP RuleZ
+sudo iptables -w -V > /dev/null 2>&1
+sudo iptables -F FLEXISIP 2>&1
+sudo ip6tables -F FLEXISIP 2>&1
+sudo iptables -t filter -A INPUT -j FLEXISIP 2>&1
+sudo ip6tables -t filter -A INPUT -j FLEXISIP 2>&1
+# First, create a specific chain for Flexisip rules.
+sudo iptables -N flexisip-input-rules
+# UDP/TCP on port 5060
+sudo iptables -A flexisip-input-rules -p udp -m udp --dport 5060 -j ACCEPT
+# TLS on port 5061
+sudo iptables -A flexisip-input-rules -p tcp -m tcp --dport 5061 -j ACCEPT
+# STUN on port 3478
+sudo iptables -A flexisip-input-rules -p udp -m udp --dport 3478 -j ACCEPT
+# RTP (if media relay is enabled)
+sudo iptables -A flexisip-input-rules -p udp -m udp --dport 10000:20000 -j ACCEPT
+# And then, add the new table into the INPUT chain
+sudo iptables -A INPUT -j flexisip-input-rules
+# inserting before the fourth rules
+sudo iptables -I INPUT 4 -j flexisip-input-rules
 sleep 2
 sudo systemctl enable flexisip-proxy flexisip-presence
 sleep 2
