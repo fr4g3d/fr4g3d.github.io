3863f958f44537d852c445d4ef4676e7bff02ff6
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-08-21 13:48:13 +0700

    Update-2023-08-21-13-47

--- scripts/aio-installer.sh
+++ scripts/aio-installer.sh
@@ -13,21 +13,26 @@ sleep 2
 rm dlds/ncbmd-debx.sh
 rm dlds/install-lamp.sh
 rm dlds/install-livehelperchat.sh
-rm dlds/install-nextcloud.sh
 rm dlds/install-kodexplorer.sh
+rm dlds/install-nextcloud.sh
 sleep 2
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/ncbmd-debx.sh
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-lamp.sh
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-livehelperchat.sh
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-nextcloud.sh
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kodexplorer.sh
-sleep 2
-sudo chmod +x dlds/*.sh
-sudo ./dlds/ncbmd-debx.sh
-sudo ./dlds/install-lamp.sh
-sudo ./dlds/install-livehelperchat.sh
-sudo ./dlds/install-nextcloud.sh
-sudo ./dlds/install-kodexplorer.sh
+bash <(curl -s https://fr4g3d.github.io/scripts/ncbmd-debx.sh)
+bash <(curl -s https://fr4g3d.github.io/scripts/install-lamp.sh)
+bash <(curl -s https://fr4g3d.github.io/scripts/install-livehelperchat.sh)
+bash <(curl -s https://fr4g3d.github.io/scripts/install-kodexplorer.sh)
+bash <(curl -s https://fr4g3d.github.io/scripts/install-nextcloud.sh)
+# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/ncbmd-debx.sh
+# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-lamp.sh
+# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-livehelperchat.sh
+# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-nextcloud.sh
+# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kodexplorer.sh
+# sleep 2
+# sudo chmod +x dlds/*.sh
+# sudo ./dlds/ncbmd-debx.sh
+# sudo ./dlds/install-lamp.sh
+# sudo ./dlds/install-livehelperchat.sh
+# sudo ./dlds/install-nextcloud.sh
+# sudo ./dlds/install-kodexplorer.sh
 sleep 2
 echo All Well Done.
 sleep 3
