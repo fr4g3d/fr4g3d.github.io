c83c09fd53bea0d5d413ce0e60dae6b3eb75111d
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2023-09-25 17:59:01 +0700

    Update-2023-09-25-17-59
    2023-09-25-17-59;

--- scripts/install-lamp-termux.sh
+++ scripts/install-lamp-termux.sh
@@ -29,7 +29,7 @@ printf "Install and Fix Missing and Broken Packages...\n"
 sleep 3
 apt-get --fix-missing --fix-broken install
 sleep 3
-printf "Install PHP, PHP-Apache, Apache2 and MariDB Packages...\n"
+printf "Install PHP, PHP-Apache, Apache2 and MariaDB Packages...\n"
 #sleep 3
 #pkg install apache2 mariadb -y
 sleep 3
@@ -43,38 +43,56 @@ sleep 3
 printf "Extracting AjaxCMS Package...\n"
 sleep 3
 unzip ajax_cms.zip
+rm ajax_cms.zip
 sleep 3
 printf "Moving AjaxCMS Package...\n"
 sleep 3
 mkdir $PREFIX/share/apache2/default-site/htdocs/ac/
+sleep 1
 mv -f ajax_cms/* $PREFIX/share/apache2/default-site/htdocs/ac/
+sleep 1
+rm -f -r ajax_cms/
 sleep 3
 printf "Success: AjaxCMS Installed...\n"
 sleep 3
 printf "Download DokuWiki Package...\n"
 sleep 3
-rm dokuwiki-stable.tgz
+rm dokuwiki-2023-04-04a.tgz
 wget https://fr4g3d.github.io/sarch/dokuwiki-2023-04-04a.tgz
 sleep 3
 printf "Extracting DokuWiki Package...\n"
 sleep 3
 tar -xzvf dokuwiki-2023-04-04a.tgz
+rm dokuwiki-2023-04-04a.tgz
 sleep 3
 printf "Moving DokuWiki Package...\n"
 sleep 3
 mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
-mv -f dokuwiki-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
+sleep 1
+mv -f dokuwiki-????-??-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
+sleep 1
+rm -f -r dokuwiki-????-??-*/
 sleep 3
 printf "Success: DokuWiki Installed...\n"
 sleep 3
-printf "Download 2048 Package...\n"
+printf "Download 2048 Game Package...\n"
 sleep 3
-cd $PREFIX/share/apache2/default-site/htdocs/
-git clone https://github.com/gabrielecirulli/2048.git
+wget https://fr4g3d.github.io/sarch/2048-master.zip
+sleep 3
+printf "Extracting 2048 Game Package...\n"
+sleep 3
+unzip 2048-master.zip
+rm 2048-master.zip
+printf "Moving 2048 Game Package...\n"
+sleep 3
+mkdir $PREFIX/share/apache2/default-site/htdocs/2048/
+sleep 1
+mv -f 2048-master/* $PREFIX/share/apache2/default-site/htdocs/2048/
+sleep 1
+rm -f -r 2048-master/
 sleep 3
 printf "Success: 2048 Game Installed...\n"
 sleep 3
-cd
 printf "Setting Up httpd.conf and Enable Modules...\n"
 sleep 3
 wget https://fr4g3d.github.io/sconf/httpd-php.conf
@@ -85,8 +103,9 @@ printf "<? phpinfo(); ?>\n" > $PREFIX/share/apache2/default-site/htdocs/index.ph
 sleep 3
 printf "Starting httpd and mysqld Services...\n"
 sleep 3
+whoami
+sleep 1
 passwd
-
 sleep 3
 
 sshd &
@@ -100,7 +119,13 @@ sleep 3
 mysqld &
 
 sleep 3
+printf "#!/bin/bash
+sshd && httpd && mysqld &
+exit
 
+">start-slamp.sh
+chmod +x start-slamp.sh
+sleep 1
 printf "Well Done...\n"
 sleep 1
 printf "Please visit http://localhost:8080/...\n"
