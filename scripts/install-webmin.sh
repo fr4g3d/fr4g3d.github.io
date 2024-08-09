8add5728d0a64d2ed8bf6676ab62336f5597bbae
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-08-09 11:56:52 +0700

    Update-09-08-24
    + Add Install WebMin Script;
    - Update index.html.

---
+++ scripts/install-webmin.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Installer WebMin
+#
+clear
+#
+
+# install common apps and Downloader.
+sudo apt-get -y install curl wget aria2 coreutils
+sleep 2
+# install WebMin.
+sudo -s
+curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sh setup-repos.sh
+sudo apt-get install webmin --install-recommends
+sleep 2
+echo Done.
+sleep 3
