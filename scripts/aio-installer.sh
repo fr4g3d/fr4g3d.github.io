a4834eacc1f5e3ae1d7fffa2a49958e17465c659
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-08-22 10:39:20 +0700

    Update-2024-08-22-10-39

--- scripts/aio-installer.sh
+++ scripts/aio-installer.sh
@@ -12,15 +12,17 @@ sudo apt-get -y install curl wget zip unzip aria2
 sleep 2
 rm dlds/ncbmd-debx.sh
 rm dlds/install-lamp.sh
-rm dlds/install-livehelperchat.sh
+rm dlds/install-speedtest.sh
 rm dlds/install-kodexplorer.sh
+rm dlds/install-kvm-cockpitvm.sh
 rm dlds/install-nextcloud.sh
+rm dlds/install-webmin.sh
 sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/ncbmd-debx.sh)
 sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/install-lamp.sh)
 sleep 2
-bash <(wget -O - https://fr4g3d.github.io/scripts/install-livehelperchat.sh)
+bash <(wget -O - https://fr4g3d.github.io/scripts/install-speedtest.sh)
 sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/install-kodexplorer.sh)
 sleep 2
@@ -29,18 +31,21 @@ sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/install-nextcloud.sh)
 sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/install-webmin.sh)
-# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/ncbmd-debx.sh
-# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-lamp.sh
-# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-livehelperchat.sh
-# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-nextcloud.sh
-# aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kodexplorer.sh
-# sleep 2
-# sudo chmod +x dlds/*.sh
-# sudo ./dlds/ncbmd-debx.sh
-# sudo ./dlds/install-lamp.sh
-# sudo ./dlds/install-livehelperchat.sh
-# sudo ./dlds/install-nextcloud.sh
-# sudo ./dlds/install-kodexplorer.sh
+sleep 2
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/ncbmd-debx.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-lamp.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-speedtest.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kodexplorer.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kvm-cockpitvm.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-nextcloud.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-webmin.sh
+sleep 2
+sudo chmod +x dlds/*.sh
+sudo bash ./dlds/ncbmd-debx.sh
+sudo bash ./dlds/install-lamp.sh
+sudo bash ./dlds/install-livehelperchat.sh
+sudo bash ./dlds/install-nextcloud.sh
+sudo bash ./dlds/install-kodexplorer.sh
 sleep 2
 echo All Well Done.
 sleep 3
---
+++ scripts/install-speedtest.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Installer LibreSpeedTest
+#
+clear
+#
+
+# install common apps and Downloader.
+sudo apt-get -y install curl wget aria2 coreutils
+sleep 2
+# Download LibreSpeedTest
+aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/spdtest.zip
+sleep 2
+# Install LibreSpeedTest
+unzip -o dlds/spdtest.zip
+sleep 2
+sudo mv -f speedtest/ /var/www/html/.apps/
+sleep 2
+echo Done.
+sleep 3
