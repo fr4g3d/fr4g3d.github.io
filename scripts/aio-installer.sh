47210c973fd936895f0be5823d3b16b9c724f994
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-08-22 15:43:45 +0700

    Update-2024-08-22-15-43

--- scripts/aio-installer.sh
+++ scripts/aio-installer.sh
@@ -12,6 +12,7 @@ sudo apt-get -y install curl wget zip unzip aria2
 sleep 2
 rm dlds/ncbmd-debx.sh
 rm dlds/install-lamp.sh
+rm dlds/install-phpmyadmin.sh
 rm dlds/install-speedtest.sh
 rm dlds/install-kodexplorer.sh
 rm dlds/install-kvm-cockpitvm.sh
@@ -21,6 +22,8 @@ sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/ncbmd-debx.sh)
 sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/install-lamp.sh)
+sleep 2
+bash <(wget -O - https://fr4g3d.github.io/scripts/install-phpmyadmin.sh)
 sleep 2
 bash <(wget -O - https://fr4g3d.github.io/scripts/install-speedtest.sh)
 sleep 2
@@ -34,6 +37,7 @@ bash <(wget -O - https://fr4g3d.github.io/scripts/install-webmin.sh)
 sleep 2
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/ncbmd-debx.sh
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-lamp.sh
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-phpmyadmin.sh
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-speedtest.sh
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kodexplorer.sh
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/scripts/install-kvm-cockpitvm.sh
@@ -43,9 +47,12 @@ sleep 2
 sudo chmod +x dlds/*.sh
 sudo bash ./dlds/ncbmd-debx.sh
 sudo bash ./dlds/install-lamp.sh
-sudo bash ./dlds/install-livehelperchat.sh
-sudo bash ./dlds/install-nextcloud.sh
+sudo bash ./dlds/install-phpmyadmin.sh
+sudo bash ./dlds/install-speedtest.sh
 sudo bash ./dlds/install-kodexplorer.sh
+sudo bash ./dlds/install-kvm-cockpitvm.sh
+sudo bash ./dlds/install-nextcloud.sh
+sudo bash ./dlds/install-webmin.sh
 sleep 2
 echo All Well Done.
 sleep 3
