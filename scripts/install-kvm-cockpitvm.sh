b43d4d4311194ac92ef17ae96ffefdc46cbc6988
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-08-23 15:26:12 +0700

    Update-2024-08-23-15-25

--- scripts/install-kvm-cockpitvm.sh
+++ scripts/install-kvm-cockpitvm.sh
@@ -11,21 +11,6 @@ sleep 2
 # install Cockpit VM App.
 sudo apt-get -y install cockpit cockpit-machines
 sleep 2
-sudo systemctl enable --now libvirtd
-sleep 2
-sudo virsh net-autostart default
-sleep 2
-sudo virsh net-start default
-sleep 2
-sudo service cockpit start
-sleep 2
-sudo usermod -aG kvm $(whoami)
-sudo usermod -aG libvirt $(whoami)
-newgrp kvm
-newgrp libvirt
-sleep 2
-sudo service cockpit restart
-sleep 2
 # install FileBrowser App.
 sudo curl -fsSL https://raw.githubusercontent.com/filebrowser/get/master/get.sh | sudo bash
 dpkg -S /usr/bin/nohup
@@ -56,6 +41,20 @@ sleep 2
 sudo service filebrowserd restart
 sleep 2
 echo Type: \"nohup filebrowser -a 0.0.0.0 -p 8787 -r /home\"
+# End Install FileBrowser App
+sleep 2
+sudo usermod -aG kvm $(whoami)
+sudo usermod -aG libvirt $(whoami)
+newgrp kvm
+newgrp libvirt
+sleep 2
+sudo systemctl enable --now libvirtd
+sleep 2
+sudo virsh net-autostart default
+sleep 2
+sudo virsh net-start default
+sleep 2
+sudo service cockpit restart
 sleep 2
 echo Done.
 sleep 3
