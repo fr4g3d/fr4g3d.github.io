79718161e3a323786a1af4174ece9e2722f937a6
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-08-27 16:55:14 +0700

    Update-2024-08-27-16-55

--- scripts/enable-fastcgi.sh
+++ scripts/enable-fastcgi.sh
@@ -12,17 +12,25 @@ sudo apt-get -y install curl wget zip unzip aria2
 sleep 2
 sudo apt update
 sleep 2
+VERSION=$(sed 's/\..*//' /etc/debian_version)
+if [[ $VERSION == '10' ]]; then
+  phpver = "7.3"
+  aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sconf/000-default10.conf
+  sudo mv -f dlds/000-default10.conf /etc/apache2/sites-available/000-default.conf
+#  bash <(wget -O - https://fr4g3d.github.io/scripts/debian-source-list-sg-buster.sh)
+elif [[ $VERSION == '11' ]]; then
+  phpver = "7.4"
+  aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sconf/000-default11.conf
+  sudo mv -f dlds/000-default11.conf /etc/apache2/sites-available/000-default.conf
+#  bash <(wget -O - https://fr4g3d.github.io/scripts/debian-source-list-sg-bullseye.sh)
+fi
 sudo apt install php php-fpm
 sleep 2
-sudo systemctl restart php7.3-fpm
+sudo systemctl restart php$phpver-fpm
 sleep 2
-sudo systemctl status php7.3-fpm
+sudo systemctl status php$phpver-fpm
 sleep 2
 sudo a2enmod alias proxy proxy_fcgi
-sleep 2
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sconf/000-default.conf
-sleep 2
-sudo mv -f dlds/000-default.conf /etc/apache2/sites-available/000-default.conf
 sleep 2
 sudo systemctl restart apache2.service
 sleep 2
