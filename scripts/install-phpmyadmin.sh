b4b4c1531e6bd91a39f71eca2874910c37b80ba1
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-09-17 14:50:17 +0700

    Update-2024-09-17-14-49

--- scripts/install-phpmyadmin.sh
+++ scripts/install-phpmyadmin.sh
@@ -17,30 +17,30 @@ sleep 2
 # install common apps and Downloader.
 sudo apt-get -y install curl wget zip unzip aria2
 sleep 2
-sudo rm -r phpMyAdmin-5.1.2-all-languages/
-sudo rm dlds/phpMyAdmin-5.1.2-all-languages.z*
+sudo rm -r phpMyAdmin-5.1.3-all-languages/
+sudo rm dlds/phpMyAdmin-5.1.3-all-languages.z*
 sudo rm -r boodark/
-#wget -P dlds/ https://files.phpmyadmin.net/phpMyAdmin/5.1.2/phpMyAdmin-5.1.2-all-languages.zip
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/phpMyAdmin-5.1.2-all-languages.zip
+#wget -P dlds/ https://files.phpmyadmin.net/phpMyAdmin/5.1.3/phpMyAdmin-5.1.3-all-languages.zip
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/phpMyAdmin-5.1.3-all-languages.zip
 sleep 2
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/boodark-1.0.1.zip
 sleep 2
-unzip dlds/phpMyAdmin-5.1.2-all-languages.zip
+unzip dlds/phpMyAdmin-5.1.3-all-languages.zip
 sleep 2
 unzip dlds/boodark-1.0.1.zip
-sudo mv -f phpMyAdmin-5.1.2-all-languages/ /usr/share/
+sudo mv -f phpMyAdmin-5.1.3-all-languages/ /usr/share/
 sleep 2
-sudo mv -f boodark/ /usr/share/phpMyAdmin-5.1.2-all-languages/themes/
-sudo chown -R www-data:www-data /usr/share/phpMyAdmin-5.1.2-all-languages/
-sudo chmod -R 755 /usr/share/phpMyAdmin-5.1.2-all-languages/
+sudo mv -f boodark/ /usr/share/phpMyAdmin-5.1.3-all-languages/themes/
+sudo chown -R www-data:www-data /usr/share/phpMyAdmin-5.1.3-all-languages/
+sudo chmod -R 755 /usr/share/phpMyAdmin-5.1.3-all-languages/
 sudo rm dlds/appz.zip
 aria2c -d dlds -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/appz.zip
 unzip -o dlds/appz.zip
-sudo cp -f .htaccess /usr/share/phpMyAdmin-5.1.2-all-languages/.htaccess
-sudo cp -f .htpasswd /usr/share/phpMyAdmin-5.1.2-all-languages/.htpasswd
+sudo cp -f .htaccess /usr/share/phpMyAdmin-5.1.3-all-languages/.htaccess
+sudo cp -f .htpasswd /usr/share/phpMyAdmin-5.1.3-all-languages/.htpasswd
 sleep 2
-sudo sh -c "printf \"Alias /pmadmin \"/usr/share/phpMyAdmin-5.1.2-all-languages\"
-<Directory /usr/share/phpMyAdmin-5.1.2-all-languages/>
+sudo sh -c "printf \"Alias /pmadmin \"/usr/share/phpMyAdmin-5.1.3-all-languages\"
+<Directory /usr/share/phpMyAdmin-5.1.3-all-languages/>
   Require all granted
   AllowOverride All
   Options FollowSymLinks MultiViews
