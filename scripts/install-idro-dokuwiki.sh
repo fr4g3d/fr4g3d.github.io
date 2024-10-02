05eb8ae84c7ff197d5d109274c44bc1bb31e6b9a
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-10-02 13:30:05 +0700

    Update-2024-10-02-13-29

--- scripts/install-idro-dokuwiki.sh
+++ scripts/install-idro-dokuwiki.sh
@@ -8,7 +8,8 @@ sleep 2
 printf "Download idRO-DokuWiki Package...\n"
 sleep 3
 rm dokuwiki-2023-04-04a.tgz
-aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/dokuwiki-2023-04-04a.tgz
+#aria2c -d dlds/ -c -s8 -j8 -x8 https://releases.wikimedia.org/mediawiki/1.38/mediawiki-1.38.7.tar.gz
+aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/mediawiki-1.38.7.tar.gz
 sleep 2
 mkdir ~/rslRO
 sleep 1
@@ -16,7 +17,9 @@ mkdir ~/rslRO/www
 sleep 1
 mkdir ~/rslRO/www/idRO-Wiki
 sleep 1
-tar -xzvf dlds/dokuwiki-2023-04-04a.tgz -C ~/rslRO/www/idRO-Wiki
+tar -xzvf dlds/dokuwiki-2023-04-04a.tgz -C ~/rslRO/www
+sleep 1
+mv ~/rslRO/www/mediawiki-1.38.7 ~/rslRO/www/idRO-Wiki
 sleep 2
 echo Well Done.
 sleep 1
