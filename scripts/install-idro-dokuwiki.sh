c32c56ab8763ef75bf5d2d7f3d8d05b4e58337e2
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2024-10-02 12:51:44 +0700

    Update-2024-10-02-12-51

--- scripts/install-idro-dokuwiki.sh
+++ scripts/install-idro-dokuwiki.sh
@@ -2,19 +2,21 @@
 #
 clear
 #
-# Install [DokuWiki]
+# Install [idRO-DokuWiki]
 sudo apt-get update
 sleep 2
-printf "Download DokuWiki Package...\n"
+printf "Download idRO-DokuWiki Package...\n"
 sleep 3
 rm dokuwiki-2023-04-04a.tgz
 aria2c -d dlds/ -c -s8 -j8 -x8 https://fr4g3d.github.io/sarch/dokuwiki-2023-04-04a.tgz
 sleep 2
 mkdir ~/rslRO
 sleep 1
-mkdir ~/rslRO/idRO-Wiki
+mkdir ~/rslRO/www
 sleep 1
-tar -xzvf dlds/dokuwiki-2023-04-04a.tgz -C ~/rslRO/idRO-Wiki
+mkdir ~/rslRO/www/idRO-Wiki
+sleep 1
+tar -xzvf dlds/dokuwiki-2023-04-04a.tgz -C ~/rslRO/www/idRO-Wiki
 sleep 2
-echo Done.
+echo Well Done.
 sleep 1
