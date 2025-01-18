12feb7399405922fe1fdc96c4e97332aa58495c9
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2025-01-18 18:53:33 +0700

    Update-2025-01-18-18-51

--- scripts/install-kvm-cockpitvm.sh
+++ scripts/install-kvm-cockpitvm.sh
@@ -9,7 +9,7 @@ clear
 sudo apt-get -y install curl wget aria2 coreutils
 sleep 2
 # install Cockpit VM App.
-sudo apt-get -y install cockpit cockpit-machines
+sudo apt-get -y install --no-install-recommends cockpit cockpit-machines qemu-system libvirt-clients libvirt-daemon-system
 sleep 2
 clear
 # install FileBrowser App.
@@ -50,6 +50,10 @@ echo Type: 'newgrp libvirt'
 sleep 2
 sudo systemctl enable --now libvirtd
 sleep 2
+#sudo ip link set down virbr0
+#sleep 2
+#sudo brctl delbr virbr0
+#sleep 2
 sudo virsh net-autostart default
 sleep 2
 sudo virsh net-start default
