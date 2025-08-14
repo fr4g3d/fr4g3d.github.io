19d3160e920ec5bfab2a34a389d68d7f83701dc3
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2025-08-14 23:38:58 +0700

    Update (2025-08-14 23:38)

--- scripts/install-lamp-termux.sh
+++ scripts/install-lamp-termux.sh
@@ -57,24 +57,24 @@ printf "Success: AjaxCMS Installed...\n"
 sleep 3
 printf "Download DokuWiki Package...\n"
 sleep 3
-rm dokuwiki-2023-04-04a.tgz
-wget https://fr4g3d.github.io/sarch/dokuwiki-2023-04-04a.tgz
-sleep 3
-printf "Extracting DokuWiki Package...\n"
-sleep 3
-tar -xzvf dokuwiki-2023-04-04a.tgz
-rm dokuwiki-2023-04-04a.tgz
-sleep 3
-printf "Moving DokuWiki Package...\n"
-sleep 3
-mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
-sleep 1
-mv -f dokuwiki-????-??-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
-sleep 1
-rm -f -r dokuwiki-????-??-*/
-sleep 3
-printf "Success: DokuWiki Installed...\n"
-sleep 3
+#rm dokuwiki-2023-04-04a.tgz
+#wget https://fr4g3d.github.io/sarch/dokuwiki-2023-04-04a.tgz
+#sleep 3
+#printf "Extracting DokuWiki Package...\n"
+#sleep 3
+#tar -xzvf dokuwiki-2023-04-04a.tgz
+#rm dokuwiki-2023-04-04a.tgz
+#sleep 3
+#printf "Moving DokuWiki Package...\n"
+#sleep 3
+#mkdir $PREFIX/share/apache2/default-site/htdocs/dw/
+#sleep 1
+#mv -f dokuwiki-????-??-*/* $PREFIX/share/apache2/default-site/htdocs/dw/
+#sleep 1
+#rm -f -r dokuwiki-????-??-*/
+#sleep 3
+#printf "Success: DokuWiki Installed...\n"
+#sleep 3
 printf "Download 2048 Game Package...\n"
 sleep 3
 wget https://fr4g3d.github.io/sarch/2048-master.zip
@@ -103,10 +103,14 @@ printf "<? phpinfo(); ?>\n" > $PREFIX/share/apache2/default-site/htdocs/index.ph
 sleep 3
 printf "Starting httpd and mysqld Services...\n"
 sleep 3
-whoami
+printf "UserName : ($whoami) \n"
+sleep 1
+printf "Password : (Change Now!)\n"
 sleep 1
 passwd
 sleep 3
+printf "Starting LAMP...\n"
+sleep 1
 
 sshd &
 
@@ -135,6 +139,8 @@ exit
 chmod +x start-slamp.sh
 sleep 1
 printf "Well Done...\n"
+sleep 1
+printf "UserName : ($whoami) \n"
 sleep 1
 printf "Please visit http://localhost:8080/...\n"
 sleep 3
