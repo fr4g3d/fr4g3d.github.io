c185d8055ac86449659109054d1fd8951b66ab6a
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2025-09-25 11:32:35 +0700

    Update-2025-09-25 11:32

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -4,6 +4,6 @@ sudo apt update
 sleep 3
 wget -O - https://get.ispconfig.org | sudo bash -s -- --use-ftp-ports=40110-40210 --unattended-upgrades
 sleep 2
-wget -N https://files.softaculous.com/install.sh
-sudo bash ./install.sh
+#wget -N https://files.softaculous.com/install.sh
+#sudo bash ./install.sh
 exit
