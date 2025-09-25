854ddc09000891b5d688b3af3e74f7403acfd869
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2025-09-26 01:27:07 +0700

    Update-2025-09-26 01:26

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -24,7 +24,7 @@ wget -qO - https://packages.sury.org/php/apt.gpg | sudo apt-key add -
 sudo apt-get update
 sleep 2
 sudo apt-get install -y ssh openssh-server nano vim-nox lsb-release apt-transport-https ca-certificates wget git gnupg software-properties-common curl cron ntp
-sudo apt-get install -y dbconfig-common postfix postfix-mysql mariadb-client mariadb-server openssl rkhunter binutils sudo getmail dovecot-imapd dovecot-pop3d dovecot-mysql dovecot-sieve dovecot-managesieved dovecot-lmtpd
+sudo apt-get install -y dbconfig-common mariadb-client mariadb-server openssl rkhunter binutils sudo getmail dovecot-imapd dovecot-pop3d dovecot-mysql dovecot-sieve dovecot-managesieved dovecot-lmtpd
 sudo apt-get install -y software-properties-common update-inetd dnsutils resolvconf clamav clamav-daemon zip unzip bzip2 xz-utils lzip borgbackup arj nomarch lzop cabextract apt-listchanges libnet-ldap-perl libauthen-sasl-perl daemon libio-string-perl libio-socket-ssl-perl libnet-ident-perl libnet-dns-perl libdbd-mysql-perl bind9 rspamd redis-server postgrey p7zip p7zip-full unrar-free lrzip
 echo "Installing PHP Old to Current..."
 sleep 2
@@ -39,7 +39,7 @@ echo "Done Preparing..."
 sleep 2
 echo "Start ISPConfig Installer..."
 sleep 2
-wget -O - https://get.ispconfig.org | sudo bash -s -- --use-ftp-ports=40110-40210 --unattended-upgrades --debug
+wget -O - https://get.ispconfig.org | sudo bash -s -- --use-ftp-ports=40110-40210 --debug
 sleep 2
 #
 #wget -N https://files.softaculous.com/install.sh
