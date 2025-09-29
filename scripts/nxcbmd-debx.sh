1d48a3117f19f9e2967b2f2165baa0d7e963ebe2
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2025-09-29 11:07:07 +0700

    Update-2025-09-29 11:06

--- scripts/nxcbmd-debx.sh
+++ scripts/nxcbmd-debx.sh
@@ -93,10 +93,10 @@ WantedBy=multi-user.target
 \" > /etc/systemd/system/sshwifty.service"
 sleep 2
 sudo systemctl enable sshwifty.service
-sleep 2
-sudo service sshwifty start
-sleep 2
-sudo service sshwifty restart
+#sleep 2
+#sudo service sshwifty start
+#sleep 2
+#sudo service sshwifty restart
 sleep 2
 echo "wget -O - https://get.ispconfig.org | sudo sh -s -- --use-ftp-ports=40110-40210 --unattended-upgrades"
 echo 
