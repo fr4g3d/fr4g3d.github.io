df2fc96f8a10ff9a90b5737542051c7cc0b5c7e5
Fr4g3d GunZ <34006122+fr4g3d@users.noreply.github.com>
2025-09-29 09:15:20 +0700

    Update-2025-09-29 09:14

--- scripts/install-ispconfig.sh
+++ scripts/install-ispconfig.sh
@@ -47,10 +47,14 @@ echo "Done Preparing..."
 sleep 2
 echo "Start ISPConfig Installer..."
 sleep 2
-wget -O - https://get.ispconfig.org | sudo bash -s -- --use-ftp-ports=40110-40210 --debug
+#wget -O - https://get.ispconfig.org | sudo bash -s -- --use-ftp-ports=40110-40210 --debug
+#wget -O - https://get.ispconfig.org | sudo bash -s -- --use-ftp-ports=40110-40210 --unattended-upgrades --debug
+wget -O - https://get.ispconfig.org | sudo bash -s -- --use-nginx --use-ftp-ports=40110-40210 --unattended-upgrades --debug
+sleep 2
+read -n 1 -s -r -p "Press Enter to continue Install Softaculous.."
 sleep 2
 #
-#wget -N https://files.softaculous.com/install.sh
-#sudo bash ./install.sh
+wget -N https://files.softaculous.com/install.sh
+sudo bash ./install.sh
 #
 exit
